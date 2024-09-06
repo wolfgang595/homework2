1a714bcb26ee5b1b163b926c0a71a641454c3cf6
Nathan Serna <nath2534@outlook.com>
2024-09-06 16:51:25 -0500

    Initial setup of working web page

---
+++ scripts/myScripts.js
@@ -0,0 +1,33 @@
+const newItem = document.getElementById("newItemTxt");
+const addButton = document.getElementById("newItemBtn");
+const pendingList = document.getElementById("pending");
+const completedList = document.getElementById("completed");
+
+addButton.addEventListener("click", addItemFunc);
+function addItemFunc(){ 
+    if(newItem.value !=""){
+        const addItem = document.createElement("li")
+        addItem.innerHTML = newItem.value
+        pendingList.appendChild(addItem)
+        newItem.value = ""
+    }
+    else {
+        alert("Please provide a task to add.")
+    }
+}
+
+pendingList.addEventListener("click", function(){ pendingEventFunc(event)})
+function pendingEventFunc(event){
+    if(event.target.localName == "li"){
+        const item = event.target
+        completedList.appendChild(item)
+    }
+}
+
+completedList.addEventListener("click",function(){completedEventFunc(event)})
+function completedEventFunc(event){
+    if(event.target.localName == "li"){
+        const item = event.target
+        pendingList.appendChild(item)
+    }
+}
